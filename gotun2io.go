38edff60c1fdf6dab4ad3199b187f1d1ba652b88
nsecgo <nsecgo@gmail.com>
2018-03-17 08:59:09 +0800

    first commit

--- gotun2io.go
+++ gotun2io.go
@@ -1,7 +1,6 @@
 package gotun2io
 
 import (
-	_ "net/http/pprof"
 	"log"
 	"github.com/google/netstack/tcpip"
 	"github.com/google/netstack/tcpip/link/fdbased"
@@ -11,15 +10,19 @@ import (
 	"github.com/google/netstack/tcpip/stack"
 	"github.com/google/netstack/tcpip/transport/tcp"
 	"github.com/google/netstack/waiter"
-	"golang.org/x/net/proxy"
-	"net"
 	"fmt"
 	"github.com/nsecgo/water"
+	"io"
 )
+
+type Dialer interface {
+	Dial(network, address string) (io.ReadWriteCloser, error)
+}
 
 const NICID = 666
 
-func Run(socks5Addr string) {
+// No block run.
+func Run(dialer Dialer) {
 	// Create the stack with ip and tcp protocols, then add a tun-based NIC and address.
 	s := stack.New([]string{ipv4.ProtocolName, ipv6.ProtocolName}, []string{tcp.ProtocolName})
 
@@ -41,11 +44,6 @@ func Run(socks5Addr string) {
 
 	s.SetPromiscuousMode(NICID, true)
 
-	dialer, err := proxy.SOCKS5("tcp", socks5Addr, nil, &net.Dialer{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	var wq waiter.Queue
 	fwd := tcp.NewForwarder(s, 0, 10, func(r *tcp.ForwarderRequest) {
 		ep, er := r.CreateEndpoint(&wq)
@@ -56,11 +54,11 @@ func Run(socks5Addr string) {
 		transportEndpointID := r.ID()
 		r.Complete(false)
 
-		socksConn, err := dialer.Dial("tcp", fmt.Sprintf("%v:%d", transportEndpointID.LocalAddress, transportEndpointID.LocalPort))
+		conn, err := dialer.Dial("tcp", fmt.Sprintf("%v:%d", transportEndpointID.LocalAddress, transportEndpointID.LocalPort))
 		if err != nil {
 			log.Fatal(err)
 		}
-		socksConn.(*net.TCPConn).SetNoDelay(false)
+		defer conn.Close()
 
 		// Create wait queue entry that notifies a channel.
 		waitEntry, notifyCh := waiter.NewChannelEntry(nil)
@@ -68,9 +66,9 @@ func Run(socks5Addr string) {
 		defer wq.EventUnregister(&waitEntry)
 
 		go func() {
-			buf := make([]byte, mtu)
+			var buf = make([]byte, mtu)
 			for {
-				n, err := socksConn.Read(buf)
+				n, err := conn.Read(buf)
 				if err != nil {
 					return
 				}
@@ -84,9 +82,9 @@ func Run(socks5Addr string) {
 					<-notifyCh
 					continue
 				}
-				return
+				break
 			}
-			socksConn.Write(v)
+			conn.Write(v)
 		}
 	})
 	s.SetTransportProtocolHandler(tcp.ProtocolNumber, fwd.HandlePacket)
